68398892d52249a14faf69fd918c28d660a31b7e
Sônia Mara de Sá <soniamarasa@hotmail.com>
2026-07-08 19:46:10 -0300

    chore: update Angular and PrimeNG dependencies, adjust build targets, and refine styles for improved theming

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
-import { LOCALE_ID, NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule, provideZoneChangeDetection } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { registerLocaleData } from '@angular/common';
 import localePT from '@angular/common/locales/pt';
-registerLocaleData(localePT);
 import { MessageService } from 'primeng/api';
 import { DialogService } from 'primeng/dynamicdialog';
+import { providePrimeNG } from 'primeng/config';
+import { ClosetTheme } from './styles/closet-theme.preset';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PagesModule } from '@pages/pages.module';
 import { TokenInterceptor } from '@interceptors/token.interceptor';
+
+registerLocaleData(localePT);
 
 @NgModule({
   declarations: [AppComponent],
@@ -18,12 +22,22 @@ import { TokenInterceptor } from '@interceptors/token.interceptor';
     BrowserModule,
     BrowserAnimationsModule,
     HttpClientModule,
-
     AppRoutingModule,
-
     PagesModule,
   ],
   providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    providePrimeNG({
+      theme: {
+        preset: ClosetTheme,
+        options: {
+          darkModeSelector: '.theme-butterfly',
+        },
+      },
+      translation: {
+        firstDayOfWeek: 0,
+      },
+    }),
     DialogService,
     {
       provide: HTTP_INTERCEPTORS,
@@ -31,7 +45,6 @@ import { TokenInterceptor } from '@interceptors/token.interceptor';
       multi: true,
     },
     { provide: LOCALE_ID, useValue: 'pt-br' },
-
     MessageService,
   ],
   bootstrap: [AppComponent],
